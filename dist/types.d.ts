8e901467b74acd3d6c44ee1001423460dd84c7d6
rich <rbrooks@gmail.com>
2020-07-28 21:51:33 -0400

    Moving types around

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -62,5 +62,16 @@ export declare type SerialData = {
         readonly [key: string]: string;
     };
 };
+export declare type Courier = {
+    readonly name: string;
+    readonly code: string;
+};
+export declare type TrackingNumber = {
+    readonly name: string;
+    readonly trackingNumber: string;
+    readonly trackingUrl: string | null;
+    readonly description: string | null;
+    readonly courier: Courier;
+};
 export {};
 //# sourceMappingURL=types.d.ts.map
--- dist/util.d.ts
+++ dist/util.d.ts
@@ -5,19 +5,8 @@ import * as ontrac from './tracking_number_data/couriers/ontrac.json';
 import * as s10 from './tracking_number_data/couriers/s10.json';
 import * as ups from './tracking_number_data/couriers/ups.json';
 import * as usps from './tracking_number_data/couriers/usps.json';
-import { TrackingCourier } from './types';
-export { amazon, dhl, fedex, ontrac, s10, ups, usps };
-export declare type Courier = {
-    readonly name: string;
-    readonly code: string;
-};
-export declare type TrackingNumber = {
-    readonly name: string;
-    readonly trackingNumber: string;
-    readonly trackingUrl: string | null;
-    readonly description: string | null;
-    readonly courier: Courier;
-};
+import { TrackingCourier, TrackingData, Courier, TrackingNumber } from './types';
+export { amazon, dhl, fedex, ontrac, s10, ups, usps, TrackingCourier, TrackingData, Courier, TrackingNumber };
 export declare const allCouriers: readonly TrackingCourier[];
 export declare const getTracking: (trackingNumber: string, couriers?: readonly TrackingCourier[] | undefined) => TrackingNumber | undefined;
 export declare const findTracking: (searchText: string, couriers?: readonly TrackingCourier[] | undefined) => readonly TrackingNumber[];
